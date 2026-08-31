b56ec1ee857f94de7c1464d5168ac8b4df607c89
nvsinha114-svg <nvsinha114@gmail.com>
2026-08-31 12:40:20 +0530

    feat: add leetcode #151 reverse words in a string

---
+++ TwoPointers/ReverseWordsInAString.java
@@ -0,0 +1,27 @@
+/*
+LeetCode #151
+Problem: Reverse Words in a String
+Difficulty: Medium
+URL: https://leetcode.com/problems/reverse-words-in-a-string/
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public String reverseWords(String s) {
+
+        String[] words = s.trim().split("\\s+");
+
+        StringBuilder sb = new StringBuilder();
+
+        
+        for(int i =words.length-1;i>=0 ;i--){
+
+        }
+    }
+            sb.append(words[i]);
+
+            if(i!=0) sb.append(" ");
+
+        return sb.toString();
+}
