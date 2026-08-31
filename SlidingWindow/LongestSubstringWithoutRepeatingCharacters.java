69d050cb023580a03db72ad527648f10bebe2f02
nvsinha114-svg <nvsinha114@gmail.com>
2026-08-31 13:04:49 +0530

    refactor: update leetcode #3 longest substring without repeating characters

--- SlidingWindow/LongestSubstringWithoutRepeatingCharacters.java
+++ SlidingWindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -9,13 +9,13 @@ Space Complexity: O(1)
 
 char ch = s.charAt(right);
 
-             if(set.contains(ch)){
+             while(set.contains(ch)){
 
                 set.remove(s.charAt(left));
                 left++;
              }
              set.add(ch);
-             counter = Math.max(counter,left+right-1);
+             counter = Math.max(counter,right-left+1);
         }
 
         return counter;
