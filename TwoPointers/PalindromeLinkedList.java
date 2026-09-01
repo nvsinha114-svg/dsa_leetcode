97ab60f8677474fa17c937a3f019d04a362d1527
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-01 15:19:16 +0530

    refactor: update leetcode #234 palindrome linked list

--- TwoPointers/PalindromeLinkedList.java
+++ TwoPointers/PalindromeLinkedList.java
@@ -7,22 +7,45 @@ Time Complexity: O(n)
 Space Complexity: O(1)
 */
 
-ListNode nextnode = current.next;
-         while(current!=null){
-             current.next =prev;
-             prev = current;
-             current =nextnode;
-        }
-           ListNode first = head;
-           ListNode second = prev;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast =head;
+        ListNode slow = head;
+        ListNode prev =null;
+        while(fast!=null && fast.next!=null){
+            slow =slow.next;
+            fast=fast.next.next;
+        }
+         ListNode current =slow;
+        
+         while(current!=null){
+            ListNode nextnode = current.next;
+             current.next =prev;
+             prev = current;
+             current =nextnode;
+        }
+           ListNode first = head;
+           ListNode second = prev;
 
-           while(second!=null){
-            if(first.val!=second.val){
-                return false;
-            }
-                first = first.next;
-                second =second.next;
-           }
+           while(second!=null){
+            
+            if(first.val!=second.val){
+                return false;
+            }
+                first = first.next;
+                second =second.next;
+           }
 
-           return true;
-    }
+           return true;
+    }
+}
