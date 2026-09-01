fad03ba2f5dde7f68253eec5db9d859dbe8bdb82
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-01 14:43:37 +0530

    feat: add leetcode #876 middle of the linked list

---
+++ TwoPointers/MiddleOfTheLinkedList.java
@@ -0,0 +1,30 @@
+/*
+LeetCode #876
+Problem: Middle of the Linked List
+Difficulty: Easy
+URL: https://leetcode.com/problems/middle-of-the-linked-list/
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
