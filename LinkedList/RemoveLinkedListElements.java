0e07f155e6efe946ed0a16310cfc33fcac6d7791
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-01 15:16:04 +0530

    Refactor removeElements method in RemoveLinkedListElements

--- LinkedList/RemoveLinkedListElements.java
+++ LinkedList/RemoveLinkedListElements.java
@@ -1,29 +1,33 @@
-/*
-LeetCode #203
-Problem: Remove Linked List Elements
-Difficulty: Easy
-URL: https://leetcode.com/problems/remove-linked-list-elements/
-Time Complexity: O(n)
-Space Complexity: O(1)
-*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
 
-while(curr.next!=null){
-        ListNode curr = dummy;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
 
-        dummy.next = head;
-        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
 
-    public ListNode removeElements(ListNode head, int val) {
-class Solution {
- */
- * }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- *     ListNode(int val) { this.val = val; }
- *     ListNode next;
- *     ListNode() {}
- *     int val;
+        while(curr.next!=null){
 
-             if(curr.next.val == val) {
+             if(curr.next.val == val) {
 
-                 curr.next = curr.next.next;
-             }
+                 curr.next = curr.next.next;
+             }
+
+             else curr = curr.next;
+        }
+
+        return dummy.next;
+        
+    }
+}
+
