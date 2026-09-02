b6e2457abbe1be4271a2a12b89ce4a82d9cef9ae
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-02 11:10:39 +0530

    refactor: update leetcode #160 intersection of two linked lists

--- TwoPointers/IntersectionOfTwoLinkedLists.java
+++ TwoPointers/IntersectionOfTwoLinkedLists.java
@@ -7,20 +7,31 @@ Time Complexity: O(n)
 Space Complexity: O(1)
 */
 
-ListNode temp1 =headA;
-     ListNode temp2 = headB;
-     while(temp1!=temp2){
-        if(temp1==null){
-         temp1 =headB;
-        }
-        else temp1=temp1.next;
-        if(temp2==null){
-            temp2 =headA;
-        }
-        else temp2 =temp2.next;
-        }
-      return temp1;
-    }
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+     ListNode temp1 =headA;
+     ListNode temp2 = headB;
+     while(temp1!=temp2){
+        if(temp1==null){
+         temp1 =headB;
+        }
+        else temp1=temp1.next;
+        if(temp2==null){
+            temp2 =headA;
+        }
+        else temp2 =temp2.next;
+        }
+      return temp1;
+    }
 }
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-public class Solution {
