0e9c20ed03dea3b7864fa2dcf0a9525fe9c31400
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-02 12:04:45 +0530

    Update RotateList.java

--- TwoPointers/RotateList.java
+++ TwoPointers/RotateList.java
@@ -1,23 +1,50 @@
-/*
-LeetCode #61
-Problem: Rotate List
-Difficulty: Medium
-URL: https://leetcode.com/problems/rotate-list/
-Time Complexity: O(n)
-Space Complexity: O(1)
-*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
 
-for(int i=0;i<steps;i++){
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
 
-            newtail = newtail.next;
-        }
+        ListNode tail = head;
+        int cnt =1;
 
-        ListNode newhead = newtail.next;
+        while(tail!=null && tail.next!=null){
 
-        newtail.next = null;
+            tail=tail.next;
+            cnt++;
+        }
 
-        return newhead;
-    
-        
-    }
+        k = k%cnt;
+
+        if(k==0) return head;
+
+        tail.next = head;
+
+        int steps = cnt-k-1;
+
+        ListNode newtail = head;
+
+        for(int i=0;i<steps;i++){
+
+            newtail = newtail.next;
+        }
+
+        ListNode newhead = newtail.next;
+
+        newtail.next = null;
+
+        return newhead;
+    
+        
+    }
 }
