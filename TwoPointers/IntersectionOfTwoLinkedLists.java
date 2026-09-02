8c206210490a8e619656836b7abe209f2caff432
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-02 11:10:33 +0530

    feat: add leetcode #160 intersection of two linked lists

---
+++ TwoPointers/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,26 @@
+/*
+LeetCode #160
+Problem: Intersection of Two Linked Lists
+Difficulty: Easy
+URL: https://leetcode.com/problems/intersection-of-two-linked-lists/
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+ListNode temp1 =headA;
+     ListNode temp2 = headB;
+     while(temp1!=temp2){
+        if(temp1==null){
+         temp1 =headB;
+        }
+        else temp1=temp1.next;
+        if(temp2==null){
+            temp2 =headA;
+        }
+        else temp2 =temp2.next;
+        }
+      return temp1;
+    }
+}
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+public class Solution {
