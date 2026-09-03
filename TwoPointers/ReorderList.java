a68c13324aab732f794fc2b23a528d379d677950
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-03 19:58:36 +0530

    refactor: update leetcode #143 reorder list

--- TwoPointers/ReorderList.java
+++ TwoPointers/ReorderList.java
@@ -7,19 +7,55 @@ Time Complexity: O(n)
 Space Complexity: O(1)
 */
 
-ListNode second = prev;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
 
-        while(second!=null){
+        ListNode slow = head;
+        ListNode fast = head;
 
-            ListNode firstNext = first.next;
-            ListNode secondNext = second.next;
+        while(fast.next!=null && fast.next.next!=null) {
 
-            first.next = second;
-            second.next = firstNext;
+            slow = slow.next;
+            fast = fast.next.next;
 
-            first = firstNext;
-            second = secondNext;
-        }
-        
-    }
+        }
+
+        ListNode prev = null;
+        ListNode curr = slow.next;
+        slow.next = null;
+
+        while(curr!=null){
+
+           ListNode newnode = curr.next;
+           curr.next = prev;
+           prev = curr;
+           curr = newnode;
+        }
+
+        ListNode first = head;
+        ListNode second = prev;
+
+        while(second!=null){
+
+            ListNode firstNext = first.next;
+            ListNode secondNext = second.next;
+
+            first.next = second;
+            second.next = firstNext;
+
+            first = firstNext;
+            second = secondNext;
+        }
+        
+    }
 }
