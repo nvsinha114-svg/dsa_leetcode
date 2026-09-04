3719a1a7fb2120b6a73c8ebcb29d41f404f2de4f
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-04 13:13:40 +0530

    feat: add leetcode #86 partition list

---
+++ TwoPointers/PartitionList.java
@@ -0,0 +1,27 @@
+/*
+LeetCode #86
+Problem: Partition List
+Difficulty: Medium
+URL: https://leetcode.com/problems/partition-list/
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+ListNode greater = greaterdummy;
+        ListNode less = lessdummy;
+
+        ListNode greaterdummy = new ListNode(0);
+        ListNode lessdummy = new ListNode(0);
+
+    public ListNode partition(ListNode head, int x) {
+class Solution {
+ */
+ * }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode() {}
+ *     ListNode next;
+ *     int val;
+ * public class ListNode {
+ * Definition for singly-linked list.
+/**
