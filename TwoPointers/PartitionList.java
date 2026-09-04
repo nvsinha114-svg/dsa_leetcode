966fac60717d268eb9ac305c4ddfc4262021db01
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-04 13:13:44 +0530

    refactor: update leetcode #86 partition list

--- TwoPointers/PartitionList.java
+++ TwoPointers/PartitionList.java
@@ -7,21 +7,45 @@ Time Complexity: O(n)
 Space Complexity: O(1)
 */
 
-ListNode greater = greaterdummy;
-        ListNode less = lessdummy;
-
-        ListNode greaterdummy = new ListNode(0);
-        ListNode lessdummy = new ListNode(0);
-
-    public ListNode partition(ListNode head, int x) {
-class Solution {
- */
- * }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- *     ListNode(int val) { this.val = val; }
- *     ListNode() {}
- *     ListNode next;
- *     int val;
- * public class ListNode {
- * Definition for singly-linked list.
 /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+
+        ListNode lessdummy = new ListNode(0);
+        ListNode greaterdummy = new ListNode(0);
+
+        ListNode less = lessdummy;
+        ListNode greater = greaterdummy;
+
+        while(head!=null){
+
+            if(head.val<x){
+
+                less.next = head;
+                less = less.next;
+            }
+
+            else {
+
+                greater.next = head;
+                greater = greater.next;
+            }
+            head = head.next;
+        }
+
+        greater.next = null;
+        less.next = greaterdummy.next;
+
+        return lessdummy.next;
+        
+    }
+}
