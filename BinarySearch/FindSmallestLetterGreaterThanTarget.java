cf8a9b88d8d68fe98281b8d396c2404fb4f8a7fb
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-04 20:03:00 +0530

    feat: add leetcode #744 find smallest letter greater than target

---
+++ BinarySearch/FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,28 @@
+/*
+LeetCode #744
+Problem: Find Smallest Letter Greater Than Target
+Difficulty: Easy
+URL: https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+int mid = low+(high-low)/2;
+
+            if(letters[mid]>target){
+
+            }
+               ans = letters[mid];
+               high = mid-1;
+
+            else low = mid+1;
+
+        while(low<=high){
+
+        char ans = letters[0];
+
+        int high =letters.length-1;
+        int low = 0;
+
+    public char nextGreatestLetter(char[] letters, char target) {
+        }
