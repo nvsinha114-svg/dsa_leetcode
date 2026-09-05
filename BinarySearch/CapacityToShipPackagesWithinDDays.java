9c58ad9748b05fd3f076ab81369747a32e667777
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-05 13:09:59 +0530

    feat: add leetcode #1011 capacity to ship packages within d days

---
+++ BinarySearch/CapacityToShipPackagesWithinDDays.java
@@ -0,0 +1,26 @@
+/*
+LeetCode #1011
+Problem: Capacity To Ship Packages Within D Days
+Difficulty: Medium
+URL: https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+int sumweights = 0;
+             int requiredays  = 0;
+
+        while(low<=high){
+       
+
+        }
+            high+=weights[i];
+
+        int high = 0;
+
+        for(int i=0;i<weights.length;i++){
+
+        }
+            low = Math.max(weights[i],low);
+
+        for(int i =0;i<weights.length;i++){
