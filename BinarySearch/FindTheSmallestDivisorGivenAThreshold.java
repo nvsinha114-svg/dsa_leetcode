529c020b26502e505f41eb578d979775ab4e9b96
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-05 13:40:54 +0530

    feat: add leetcode #1283 find the smallest divisor given a threshold

---
+++ BinarySearch/FindTheSmallestDivisorGivenAThreshold.java
@@ -0,0 +1,28 @@
+/*
+LeetCode #1283
+Problem: Find the Smallest Divisor Given a Threshold
+Difficulty: Medium
+URL: https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+int mid = low + (high-low)/2;
+
+            for(int i =0 ;i<nums.length;i++){
+
+                sum+=(nums[i]+mid-1)/mid;
+            int sum = 0;
+
+        while(low<=high){
+
+        }
+            high = Math.max(nums[i],high);
+
+        for(int i = 0;i<nums.length;i++){
+
+        int high = Integer.MIN_VALUE;
+        int low = 1;
+
+    public int smallestDivisor(int[] nums, int threshold) {
+            }
