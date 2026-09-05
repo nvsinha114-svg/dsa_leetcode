2c6a98fc6f117149a638309562df49daf63a7865
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-05 12:20:15 +0530

    feat: add leetcode #1323 maximum 69 number

---
+++ Greedy/Maximum69Number.java
@@ -0,0 +1,26 @@
+/*
+LeetCode #1323
+Problem: Maximum 69 Number
+Difficulty: Easy
+URL: https://leetcode.com/problems/maximum-69-number/
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+public int maximum69Number (int num) {
+
+             char[] arr = String.valueOf(num).toCharArray();
+
+             for(int i=0;i<arr.length;i++){
+
+             }
+                if(arr[i]=='6'){
+                   
+                }
+    }
+                   arr[i] = '9';
+                   break;
+
+        
+        return Integer.parseInt(new String(arr));
+}
