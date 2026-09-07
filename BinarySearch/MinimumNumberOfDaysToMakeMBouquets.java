6e6d1df12da43eba7d3f55ba6609fa97506575ff
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-07 16:29:33 +0530

    feat: add leetcode #1482 minimum number of days to make m bouquets

---
+++ BinarySearch/MinimumNumberOfDaysToMakeMBouquets.java
@@ -0,0 +1,29 @@
+/*
+LeetCode #1482
+Problem: Minimum Number of Days to Make m Bouquets
+Difficulty: Medium
+URL: https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+ans = mid;
+            if(canMake(bloomDay,m , k , mid)){
+
+            int mid =low+(high-low)/2;
+
+        while(low<=high){
+
+        int ans =-1;
+
+        }
+            high = Math.max(bloom,high);
+
+        for(int bloom : bloomDay){
+
+        int high = Integer.MIN_VALUE;
+        int low = 1;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
