daf0ca5c6965622d5395f1fed9e3dd4106993c14
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-08 12:44:53 +0530

    feat: add leetcode #231 power of two

---
+++ Arrays/PowerOfTwo.java
@@ -0,0 +1,15 @@
+/*
+LeetCode #231
+Problem: Power of Two
+Difficulty: Easy
+URL: https://leetcode.com/problems/power-of-two/
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+      if(n<=0)return false;
+      return(n & n-1)==0;
+    }
+}
