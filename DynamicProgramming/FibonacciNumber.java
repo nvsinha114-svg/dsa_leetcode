e83a135f0fe6ce873485d3e6a8486d1072bf7ca9
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-08 12:42:02 +0530

    feat: add leetcode #509 fibonacci number

---
+++ DynamicProgramming/FibonacciNumber.java
@@ -0,0 +1,30 @@
+/*
+LeetCode #509
+Problem: Fibonacci Number
+Difficulty: Easy
+URL: https://leetcode.com/problems/fibonacci-number/
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+
+    public int fib(int n) {
+
+        if(n<=1)
+            return n;
+
+        int prev2=0;
+        int prev1=1;
+
+        for(int i=2;i<=n;i++){
+
+            int curr=prev1+prev2;
+
+            prev2=prev1;
+            prev1=curr;
+        }
+
+        return prev1;
+    }
+}
