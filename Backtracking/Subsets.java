adba93bf961d402a156006a2f705c15343bea06b
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-08 13:37:15 +0530

    refactor: update leetcode #78 subsets

--- Backtracking/Subsets.java
+++ Backtracking/Subsets.java
@@ -7,18 +7,34 @@ Time Complexity: O(n)
 Space Complexity: O(1)
 */
 
-}
-            return;
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
 
-        //lo
-        current.add(nums[index]);
-        backtrack(nums,index+1,current,ans);
+        List<List<Integer>> ans = new ArrayList<>();
 
-        //backtrack
-        current.remove(current.size()-1);
+        backtrack(nums,0,new ArrayList<>(),ans);
+        
+        return ans;
+        
+    }
 
-        //mat lo
-        backtrack(nums,index+1,current,ans);
+    public void backtrack(int[] nums,int index ,List<Integer> current,List<List<Integer>> ans){
 
-    }
+        if(index==nums.length){
+
+            ans.add(new ArrayList<>(current));
+            return;
+        }
+
+        //lo
+        current.add(nums[index]);
+        backtrack(nums,index+1,current,ans);
+
+        //backtrack
+        current.remove(current.size()-1);
+
+        //mat lo
+        backtrack(nums,index+1,current,ans);
+
+    }
 }
