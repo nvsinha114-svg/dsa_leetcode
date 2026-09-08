d23c16e77a0cfaffbae01f369abe23dfc7ae6427
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-08 13:05:14 +0530

    refactor: update leetcode #50 pow(x, n)

--- Arrays/PowXN.java
+++ Arrays/PowXN.java
@@ -7,22 +7,29 @@ Time Complexity: O(n)
 Space Complexity: O(1)
 */
 
-class Solution {
-    public double myPow(double x, int n) {
-        
-    }
+class Solution {
+    public double myPow(double x, int n) {
 
-    public double power(double x , int n){
+        int N = n;
 
-        if(n==0) return 1;
+        if(N<0){
 
+            x=1/x;
+            N= -N;
+        }
 
-        int N = n;
+        return power(x,N);
+        
+    }
 
-        if(N<0){
+    public double power(double x , int n){
 
-        }
-            x=1/x;
-            N= -N;
+        if(n==0) return 1;
 
-        return power(x,N);
+        double half = power(x,n/2);
+
+        if(n%2==0) return half*half;
+
+        return half*half*x;
+    }
+}
