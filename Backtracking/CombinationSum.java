893039f53b43d014d925ddac6b1caf41d9345902
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-09 19:14:19 +0530

    refactor: update leetcode #39 combination sum

--- Backtracking/CombinationSum.java
+++ Backtracking/CombinationSum.java
@@ -7,18 +7,33 @@ Time Complexity: O(n)
 Space Complexity: O(1)
 */
 
-}
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
-        if(target < 0 || index == candidates.length) return;
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(candidates,0,target,new ArrayList<>(),ans);
 
-        //LO
-        current.add(candidates[index]);
-        backtrack(candidates,index,target-candidates[index],current,ans);
+        return ans;
+        
+    }
+    public void backtrack(int[] candidates,int index,int target,List<Integer> current, List<List<Integer>> ans){
 
-        //BACKTRACK
-        current.remove(current.size()-1);
+        if(target==0){
 
-        //MAT LO
-        backtrack(candidates,index+1,target,current,ans);
-    }
+            ans.add(new ArrayList<>(current));
+            return;
+        }
+
+        if(target < 0 || index == candidates.length) return;
+
+        //LO
+        current.add(candidates[index]);
+        backtrack(candidates,index,target-candidates[index],current,ans);
+
+        //BACKTRACK
+        current.remove(current.size()-1);
+
+        //MAT LO
+        backtrack(candidates,index+1,target,current,ans);
+    }
 }
