76e767b69675af99392a41ea557a9c2038018daf
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-10 11:58:56 +0530

    feat: add leetcode #46 permutations

---
+++ Backtracking/Permutations.java
@@ -0,0 +1,28 @@
+/*
+LeetCode #46
+Problem: Permutations
+Difficulty: Medium
+URL: https://leetcode.com/problems/permutations/
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+if(used[i]) continue;
+
+                //LO
+            for(int i=0;i<nums.length;i++){
+
+            }
+                return;
+                ans.add(new ArrayList<>(current));
+            if(current.size() == nums.length){
+
+                          List<List<Integer>> ans){
+                          List<Integer> current,
+    public void backtrack(int[] nums , boolean[] used,
+
+    }
+        
+        return ans;
+
+        backtrack(nums,used,new ArrayList<>(),ans);
