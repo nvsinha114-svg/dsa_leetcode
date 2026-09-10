1c640ebda73ce2fccb754d7c6aa45c0901c0acb6
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-10 12:42:16 +0530

    refactor: update leetcode #77 combinations

--- Backtracking/Combinations.java
+++ Backtracking/Combinations.java
@@ -7,22 +7,36 @@ Time Complexity: O(n)
 Space Complexity: O(1)
 */
 
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+
+        List<List<Integer>> ans  = new ArrayList<>();
+
+        backtrack(n,k,1,new ArrayList<>(),ans);
+
+        return ans;
+        
+    }
+
+    public void backtrack(int n, int k , int start,
+                          List<Integer> current,
+                          List<List<Integer>> ans){
+
+        if(current.size() == k){
+            ans.add(new ArrayList<>(current));
+            return;
+        }
+
+        for(int i=start;i<=n;i++){
+
+            //LO
+            current.add(i);
+            backtrack(n,k,i+1,current,ans);
+
+            //MAT LO
+            current.remove(current.size()-1);
+        }
+        
+
+    }
 }
-            return;
-            ans.add(new ArrayList<>(current));
-        if(current.size() == k){
-
-                          List<List<Integer>> ans){
-                          List<Integer> current,
-    public void backtrack(int n, int k , int start,
-
-    }
-        
-        return ans;
-
-        backtrack(n,k,1,new ArrayList<>(),ans);
-        List<List<Integer>> ans  = new ArrayList<>();
-
-
-    public List<List<Integer>> combine(int n, int k) {
-class Solution {
