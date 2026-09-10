f8cebf92237f99197db7d337d16604cf8f3a42f5
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-10 11:59:02 +0530

    refactor: update leetcode #46 permutations

--- Backtracking/Permutations.java
+++ Backtracking/Permutations.java
@@ -7,22 +7,40 @@ Time Complexity: O(n)
 Space Complexity: O(1)
 */
 
-if(used[i]) continue;
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
 
-                //LO
-            for(int i=0;i<nums.length;i++){
+        List<List<Integer>> ans = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
 
-            }
-                return;
-                ans.add(new ArrayList<>(current));
-            if(current.size() == nums.length){
+        backtrack(nums,used,new ArrayList<>(),ans);
 
-                          List<List<Integer>> ans){
-                          List<Integer> current,
-    public void backtrack(int[] nums , boolean[] used,
+        return ans;
+        
+    }
 
-    }
-        
-        return ans;
+    public void backtrack(int[] nums , boolean[] used,
+                          List<Integer> current,
+                          List<List<Integer>> ans){
 
-        backtrack(nums,used,new ArrayList<>(),ans);
+            if(current.size() == nums.length){
+                ans.add(new ArrayList<>(current));
+                return;
+            }
+
+            for(int i=0;i<nums.length;i++){
+
+                if(used[i]) continue;
+
+                //LO
+                used[i] = true;
+                current.add(nums[i]);
+                backtrack(nums,used,current,ans);
+
+                //MAT LO 
+                current.remove(current.size()-1);
+                used[i] = false;
+            }
+                        
+      }
+}
