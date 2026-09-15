3744c5da6c95e3e02137bd8eed7b1d34254f4641
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-15 12:32:19 +0530

    feat: add leetcode #933 number of recent calls

---
+++ Queue/NumberOfRecentCalls.java
@@ -0,0 +1,27 @@
+/*
+LeetCode #933
+Problem: Number of Recent Calls
+Difficulty: Easy
+URL: https://leetcode.com/problems/number-of-recent-calls/
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+q.offer(t);
+
+        while(q.peek()<t-3000){
+
+        }
+            q.remove();
+
+    public int ping(int t) {
+    
+    }
+        
+        q = new LinkedList<>();
+    public RecentCounter() {
+
+
+    Queue<Integer> q;
+
+class RecentCounter {
