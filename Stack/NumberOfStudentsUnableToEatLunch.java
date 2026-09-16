f8eb5506a3c1d3cfabfe9b813340b5a5f5127f87
nvsinha114-svg <nvsinha114@gmail.com>
2026-09-16 11:56:25 +0530

    refactor: update leetcode #1700 number of students unable to eat lunch

--- Stack/NumberOfStudentsUnableToEatLunch.java
+++ Stack/NumberOfStudentsUnableToEatLunch.java
@@ -7,24 +7,43 @@ Time Complexity: O(n)
 Space Complexity: O(1)
 */
 
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+
+        Queue<Integer> q = new LinkedList<>();
+        Stack<Integer> st = new Stack<>();
+
+        for(int x : students){
+
+            q.offer(x);
+        }
+
+        for(int i =sandwiches.length-1;i>=0;i--){
+
+            st.push(sandwiches[i]);
+        }
+
+        int cnt= 0;
+
+        while(!q.isEmpty()){
+
+            if(q.peek()!=st.peek()){
+
+                cnt++;
+                q.offer(q.poll());
+                if(cnt==q.size()) break;
+            }
+
+            else {
+
+                q.poll();
+                st.pop();
+                cnt=0;
+                
+            }
+        }
+
+        return q.size();
+        
+    }
 }
-
-            else {
-
-                q.poll();
-                st.pop();
-                cnt=0;
-                
-            }
-        }
-
-        return q.size();
-                q.offer(q.poll());
-        
-                cnt++;
-
-            if(q.peek()!=st.peek()){
-
-        while(!q.isEmpty()){
-
-                if(cnt==q.size()) break;
